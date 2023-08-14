2a2bd2770a2a1ed8737631d3510e4dc4ee5773b3
harkato <92404938+harkato@users.noreply.github.com>
2023-08-14 14:23:54 -0300

    Create html display of the calculator

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ const divide = function(num1, num2) {
     return num1 / num2;
 };
 
+let num1 = 0;
+let operator = "";
+let num2 = 0;
+
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return "Invalid operator";
+    }
+}
